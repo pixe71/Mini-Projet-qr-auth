dbaa50016ff16730a86bf23acc304114a41936a0
Luc Tournie <luc.tourniem@gmail.com>
2025-12-17 19:02:14 +0100

    sql update

--- BDD.sql
+++ BDD.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.3
+-- version 5.2.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 172.16.10.220:3306
--- Généré le : mar. 09 déc. 2025 à 15:11
--- Version du serveur : 12.1.2-MariaDB-ubu2404
--- Version de PHP : 8.4.14
+-- Hôte : db:3306
+-- Généré le : mer. 17 déc. 2025 à 18:01
+-- Version du serveur : 11.8.2-MariaDB-ubu2404
+-- Version de PHP : 8.2.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,8 +40,7 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id`, `username`, `password`, `a2f_token`, `a2f_expiration`) VALUES
-(3, 'Luc', '$2y$10$tFAi4WcacvhPCC6ljc0j5ObC5Q69RLClzhGU897gGxD0giiDodV.u', NULL, '2025-12-09 14:08:22'),
-(5, 'Raf', '$2y$10$kQK/R7e3IUnSSA9na.u1UekQ5SKqgriVzXkJU7lL1vTOV7I9jVYQa', NULL, '2025-12-02 14:20:33');
+(3, 'Test', '$2y$10$tFAi4WcacvhPCC6ljc0j5ObC5Q69RLClzhGU897gGxD0giiDodV.u', NULL, '2025-12-17 18:00:56');
 
 -- --------------------------------------------------------
 
@@ -51,7 +50,7 @@ INSERT INTO `admins` (`id`, `username`, `password`, `a2f_token`, `a2f_expiration
 
 CREATE TABLE `reservations` (
   `id` int(11) NOT NULL,
-  `numero_reservation` varchar(20) NOT NULL,
+  `numero_reservation` varchar(20) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   `nom_complet` varchar(100) NOT NULL,
   `date_reservation` date NOT NULL,
@@ -59,18 +58,6 @@ CREATE TABLE `reservations` (
   `heure_fin` time NOT NULL,
   `motif` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `rfid_logs`
---
-
-CREATE TABLE `rfid_logs` (
-  `id` int(11) NOT NULL,
-  `uid` varchar(50) NOT NULL,
-  `timestamp` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 -- --------------------------------------------------------
@@ -85,10 +72,17 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `nom_complet` varchar(100) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
   `a2f_token` varchar(20) DEFAULT NULL,
-  `a2f_expiration` datetime DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT current_timestamp()
+  `a2f_expiration` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `nom_complet`, `email`, `created_at`, `a2f_token`, `a2f_expiration`) VALUES
+(3, 'test1', '$2y$10$SyUcBuWs.RuncZgi3HzvB.9P3AV/tPMBJXC2wBEJYTEkQgNpuVvnG', 'test', 'tset@test.com', '2025-12-17 18:00:24', NULL, '2025-12-17 18:02:36');
 
 -- --------------------------------------------------------
 
@@ -121,15 +115,9 @@ ALTER TABLE `admins`
 --
 ALTER TABLE `reservations`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `numero_reservation` (`numero_reservation`),
   ADD UNIQUE KEY `unique_slot` (`date_reservation`,`heure_debut`),
+  ADD UNIQUE KEY `numero_reservation` (`numero_reservation`),
   ADD KEY `user_id` (`user_id`);
-
---
--- Index pour la table `rfid_logs`
---
-ALTER TABLE `rfid_logs`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `users`
@@ -152,31 +140,25 @@ ALTER TABLE `users_rfid`
 -- AUTO_INCREMENT pour la table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `rfid_logs`
---
-ALTER TABLE `rfid_logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT pour la table `users_rfid`
 --
 ALTER TABLE `users_rfid`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Contraintes pour les tables déchargées
@@ -186,7 +168,7 @@ ALTER TABLE `users_rfid`
 -- Contraintes pour la table `reservations`
 --
 ALTER TABLE `reservations`
-  ADD CONSTRAINT `1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
